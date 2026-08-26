0250e0e70cbc4f8eeac10519e914d36a55519b2f
yash55-sop <ys5757556@gmail.com>
2026-08-26 18:35:15 +0530

    Time: 0 ms (100%), Space: 14 MB (57.39%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        vector<string> rows(numRows);
+        int currentRow = 0;
+        int direction = 1;
+
+        for (char ch : s) {
+            rows[currentRow] += ch;
+
+            if (currentRow == 0) {
+                direction = 1;     
+            } 
+            else if (currentRow == numRows - 1) {
+                direction = -1;     
+            }
+
+            currentRow += direction;
+        }
+
+        string result;
+        for (const string& row : rows) {
+            result += row;
+        }
+
+        return result;
+    }
+};
