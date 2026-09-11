f3516760e8beb863d5956e94593ce26d97da4ee4
yash55-sop <ys5757556@gmail.com>
2026-09-11 21:27:37 +0530

    Time: 0 ms (100%), Space: 95.8 MB (25.14%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if (nums1.size() > nums2.size())
-            return findMedianSortedArrays(nums2, nums1);
+        vector<int> arr;
 
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        int i = 0, j = 0;
 
-        int low = 0, high = n1;
-
-        while (low <= high) {
-            int cut1 = (low + high) / 2;
-            int cut2 = (n1 + n2 + 1) / 2 - cut1;
-
-            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
-            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
-
-            int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
-            int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
-
-            if (l1 <= r2 && l2 <= r1) {
-                if ((n1 + n2) % 2 == 0) {
-                    return (max(l1, l2) + min(r1, r2)) / 2.0;
-                } else {
-                    return max(l1, l2);
-                }
-            }
-            else if (l1 > r2) {
-                high = cut1 - 1;
-            }
-            else {
-                low = cut1 + 1;
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                arr.push_back(nums1[i]);
+                i++;
+            } else {
+                arr.push_back(nums2[j]);
+                j++;
             }
         }
 
-        return 0.0;
+        while (i < nums1.size()) {
+            arr.push_back(nums1[i]);
+            i++;
+        }
+
+        while (j < nums2.size()) {
+            arr.push_back(nums2[j]);
+            j++;
+        }
+
+        int n = arr.size();
+
+        if (n % 2 == 1) {
+            return arr[n / 2];
+        }
+
+        return (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
     }
 };
